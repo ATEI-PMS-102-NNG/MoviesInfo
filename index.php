ed73d9919bfab566cb28f2285289dfcab7121cc0
kouskou <32963289+kouskou@users.noreply.github.com>
2017-11-21 13:48:45 +0200

    Our main php file

--- index.php
+++ index.php
@@ -5,23 +5,23 @@
     <title>MoviesInfo</title>
     <link rel="stylesheet" type="text/css" href="MoviesInfo.css">
     <script src="jquery-3.2.1.js"></script>
-    <!-- Script που θα πάρει το πεδίο που θα πληκτρολογήσει ο χρήστης για να ψάξει για την ταινία - δε δουλεύει!!
+
     <script>
-        function movieSearch(input){
+        //Auto douleuei
+        //document.getElementById("demo").innerHTML = movieSearch(userInput);
+        function movieSearch(){
+            var userInput = document.getElementById("search1").value;
+            var a = document.getElementById("myID");
+            a.href += userInput;
+            document.getElementById("submit1").onclick = self.location=a.href;
 
         }
     </script>
 
-    <!-- Jquery ερώτημα για να αλλάξει τη το περιεχόμενο του jReturn div - δε δουλεύει!!
-    $(document).ready(function(){
-        $("submit1").click(function(){
-            $("#jReturn").append("a");
-        });
-    });-->
-
 </head>
 
 <body>
+
 
 <!-- Εδώ βρίσκονται ο τίτλος και το logo που πατώντας το μας πηγαινει στο site τους -->
 <div class = "TheHeader">
@@ -31,60 +31,33 @@
     <h1>Movie Info</h1><br>
 </div>
 
-<p>Learn all about your favourite movies!</p><br>
+<p>Learn all about yout favourite movies!</p><br>
 
 <!-- Εδώ βρίσκεται το search box και το button για την εκκίνηση αναζήτησης -->
 <div class = "TheSearch">
-    <form id="searchMovie" method="post">
-        <!-- Προσπάθεια να δουλέψει η αναζήτηση με το action αλλά δεν μπορώ να πάρω δυναμικά το πεδίο που πληκτρολογεί ο χρήστης
-        action="https://api.themoviedb.org/3/search/movie?api_key=b5456db86ace1556b60313e04972fc9f&language=en-US&query=thor&page=1&include_adult=false"> -->
-        <input type="text" id="search1" name="search" placeholder="Start Searching!">
-        <input type="submit" id="submit1" value="Submit" >
-        <!-- Με αυτό θα ενεργοποιηθεί η μέθοδος για να ψάξει την ταινία onclick="movieSearch(document.getElementById('search1').value)"> -->
+    <form>
+        <input type="text" id="search1" name="search" autocomplete="off" placeholder="Start Searching!">
+        <input type="button" id="submit1" value="Submit" onclick="movieSearch()">
+        <input type="button" id="submit2" value="Submit2">
     </form>
 </div><br>
 
 <div class = "FrontPage">
     <table>
-        <tr>
-            <td><a href="https://api.themoviedb.org/3/movie/tt3501632?api_key=b5456db86ace1556b60313e04972fc9f&append_to_response=videos,images">Information about Thor movie</a></td>
+
+        <tr class="collapse"><!-- Αυτο το εχουμε κανει να μη φαινεται -->
+            <td><a id="myID" href="https://api.themoviedb.org/3/search/movie?api_key=b5456db86ace1556b60313e04972fc9f&query=">The Search link</a></td>
         </tr>
         <tr>
-            <td><a href = "http://api.themoviedb.org/3/movie/47933?api_key=b5456db86ace1556b60313e04972fc9f"> Information for Independence Day Collection</a></td>
+            <td><a href="https://api.themoviedb.org/3/movie/popular?api_key=b5456db86ace1556b60313e04972fc9f&language=en-US&page=1">popular movies!</a></td>
         </tr>
-        <tr>
-            <td><a href="https://api.themoviedb.org/3/genre/movie/list?api_key=b5456db86ace1556b60313e04972fc9f&callback=jQuery22408132628371243029_1471207855881&_=1471207855884">a test with a jquery</a></td>
-        </tr>
-        <tr>
-            <td><a href="https://api.themoviedb.org/3/search/movie?api_key=b5456db86ace1556b60313e04972fc9f&query=Jack+Reacher">Information for Jack Reacher movie</a></td>
-        </tr>
-        <tr>
-            <td><a id="searchLink" href="https://api.themoviedb.org/3/search/movie?api_key=b5456db86ace1556b60313e04972fc9f&query=userInput">a test with a query</a></td>
-        </tr>
-        <tr>
-            <td><a href="http://api.themoviedb.org/3/movie/47933/images?api_key=b5456db86ace1556b60313e04972fc9f">information for images</a></td><!--nomizw lathos proseggish gia eikona-->
-        </tr>
-        <tr>
-            <td><a href="https://api.themoviedb.org/3/find/tt0944947?api_key=b5456db86ace1556b60313e04972fc9f&external_source=imdb_id">information from external source</a></td><!--nomizw lathos proseggish gia eikona-->
-        </tr>
-        <tr>
-            <td><a href="https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg">image from Mad Max</a></td>
-        </tr>
+
     </table>
 </div>
 
-<div id="jReturn">
-
-</div>
 
 <footer></footer>
 
-
-
-<!-- https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg -->
-<!-- https://api.themoviedb.org/3/configuration?api_key=<<api_key>> -->
-<!-- https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false -->
-<!-- our API key is : b5456db86ace1556b60313e04972fc9f -->
 </body>
 
 </html>
