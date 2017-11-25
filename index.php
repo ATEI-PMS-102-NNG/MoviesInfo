9a797b7b40e7a256dba521fcdba491309757378d
kouskou <32963289+kouskou@users.noreply.github.com>
2017-11-25 17:35:30 +0200

    Using ajax and get method from TMDB APPIs documentation, getting the title from a movie that the user has typed.

--- index.php
+++ index.php
@@ -2,40 +2,50 @@
 <html>
 
 <head>
-
-
-
     <title>MoviesInfo</title>
     <link rel="stylesheet" type="text/css" href="MoviesInfo.css">
+    <meta charset="utf-8"/>
     <script src="jquery-3.2.1.js"></script>
 
-    <script type="text/javascript">
-        //Auto douleuei
-        //document.getElementById("demo").innerHTML = movieSearch(userInput);
-        function movieSearch(userInput){
-            var urlObj = {href:"https://api.themoviedb.org/3/search/movie?api_key=b5456db86ace1556b60313e04972fc9f&query="}; //το url από το API που θα γίνει η κλήση (χωρίς το κείμενο του χρήστη)
-            urlObj.href += userInput; //πρόσθεση κειμένου χρήστη στο url για αναζήτηση ταινίας
-            document.getElementById("submit1").onclick = self.location= urlObj.href; //με το κλικ στο submit1 γίνεται η κλήση στο API με το ολοκληρωμένο url
-            /*για να πάρουμε το JSON - δε δουλεύει!!!
-            var myjson;
-            $.getJSON("https://api.themoviedb.org/3/movie/550?api_key=b5456db86ace1556b60313e04972fc9f&callback=test", function(json){
-                myjson = json;
+    <script>
+
+
+        //Paradeigma xmlHTTP Request apo to documentation tou TMDB API
+        function f1(){
+
+            var userInput = document.getElementById("search1").value;
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://api.themoviedb.org/3/search/movie?api_key=b5456db86ace1556b60313e04972fc9f&query="+userInput,
+                "method": "GET",
+                "headers": {},
+                "data": "{}"
+            };
+
+
+
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+                document.getElementById("results").innerHTML = response.results[0].original_title;
             });
-            console.log(myjson);*/
+
+
         }
+
+
+
     </script>
 
-    <script>
-        //script που τρέχει όταν πατηθεί το enter (κάνει search)
-        window.addEventListener('keyup', function (e) {
-            if (e.keyCode === 13) {
-                movieSearch(document.getElementById("search1").value);
-            }
-        });
-    </script>
+
 </head>
 
+
+
 <body>
+
+
+
 
 
 <!-- Εδώ βρίσκονται ο τίτλος και το logo που πατώντας το μας πηγαινει στο site τους -->
@@ -46,25 +56,38 @@
     <h1>Movie Info</h1><br>
 </div>
 
-<p><h2>Learn all about your favourite movies!</h2></p><br>
+<p>Learn all about yout favourite movies!</p><br>
 
 <!-- Εδώ βρίσκεται το search box και το button για την εκκίνηση αναζήτησης -->
 <div class = "TheSearch">
     <form>
         <input type="text" id="search1" name="search" autocomplete="off" placeholder="Start Searching!">
-        <input type="button" id="submit1" value="Search" onclick="movieSearch(document.getElementById('search1').value)">
+        <input type="button" id="submit" value="Submit" onclick="f1()">
     </form>
 </div><br>
 
 <div class = "FrontPage">
     <table>
+
+        <tr class="collapse"><!-- Αυτο το εχουμε κανει να μη φαινεται -->
+            <td><a id="myID" href="https://api.themoviedb.org/3/search/movie?api_key=b5456db86ace1556b60313e04972fc9f&query=">The Search link</a></td>
+        </tr>
         <tr>
             <td><a href="https://api.themoviedb.org/3/movie/popular?api_key=b5456db86ace1556b60313e04972fc9f&language=en-US&page=1">popular movies!</a></td>
+        </tr>
+        <tr>
+            <td><a href="https://api.themoviedb.org/3/movie/157336?api_key=b5456db86ace1556b60313e04972fc9f&append_to_response=videos,images"> Videos and Images... </a></td>
+
         </tr>
 
     </table>
 </div>
 
+<br>
+<!-- Edw emfanizetai o titlos tainias -->
+<div class="display" id="results">
+
+</div>
 
 <footer></footer>
 
