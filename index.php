c04363c1defcf40b3f2460f71343c56306c7055d
kouskou <32963289+kouskou@users.noreply.github.com>
2017-11-29 16:01:36 +0200

    Making a DIV dynamically in witch we display movie information. That information, is also being created dynamically: the title, poster and overview of a movie.

--- index.php
+++ index.php
@@ -13,8 +13,6 @@
         //jquery Request apo to documentation tou TMDB API
         function f1(){
 
-            //document.getElementById(results).style.visibility = "visible";
-
             var userInput = document.getElementById("search1").value;
             var settings = {
                 "async": true,
@@ -26,33 +24,57 @@
             };
 
 
-
             $.ajax(settings).done(function (response) {
 
                 console.log(response);
                 var i;
                 var obj1, obj2, obj3;
-                var myResult = "";
+                var myResult1, myResult2, myResult3="";
+                var movieWrapper;
 
-                for(i=0; i<response.results.length; i++){
+                for(i=0; i<3; i++){
+                    //Making a DIV dynamically. In this DIV, the title, poster and overview of a movie is being displayed.
+                    movieWrapper = document.createElement("DIV");
+                    movieWrapper.id = "displayDiv";
+                    movieWrapper.style.textAlign = "center";
+                    movieWrapper.style.fontSize = "100%";
+                    movieWrapper.style.backgroundColor = "#f2f2f2";
+                    movieWrapper.style.margin = "auto";
+                    movieWrapper.style.width = "70%";
+                    movieWrapper.style.border = "3px solid #73AD21";
+                    document.body.appendChild(movieWrapper);
 
-                   // myResult += '<div class="div-container">';
-                   // myResult += '<p class="p-title"> movie title: ' + response.results[i].original_title + '</p>';
-                   // myResult += '<img class="poster" src="https://image.tmdb.org/t/p/w500/">' + response.results[i].poster_path;
-                   // myResult += '<p class="p-overview">movie overview: ' +  response.results[i].overview + '</p>';
-                   // myResult += '</div>';
+                    //creating a <p> for the title and appending it into the DIV
+                    obj1 = response.results[i].original_title; //getting the original_title from table results that is in the returned Json data
+                    myResult1 = document.createElement("P");
+                    myResult1.innerHTML = obj1;
+                    document.getElementById("displayDiv").appendChild(myResult1);
 
-                   obj1 += response.results[i].original_title + "<br>";
+                    //creating an <img> for the poster and appending it into the DIV
+                    obj2 = response.results[i].poster_path; //getting the poster_path from table results that is in the returned Json data
+                    myResult2 = document.createElement("IMG");
+                    myResult2.src = "https://image.tmdb.org/t/p/w500/";
+                    myResult2.src += obj2;
+                    document.getElementById("displayDiv").appendChild(myResult2);
 
-                   obj2 += response.results[i].poster_path + "<br>";
+                    //creating a <p> for the overview and appending it into the DIV
+                    obj3 = response.results[i].overview; //getting the overview from table results that is in the returned Json data
+                    myResult3 = document.createElement("P");
+                    myResult3.innerHTML = obj3;
+                    document.getElementById("displayDiv").appendChild(myResult3);
 
-                  obj3 +=  response.results[i].overview + "<br>";
+                    //here is a non-dynamically approach
+                  //  obj1 += response.results[i].original_title + "<br>";
+
+                  // obj2 += response.results[i].poster_path + "<br>";
+
+                  // obj3 +=  response.results[i].overview + "<br>";
 
                 }
 
-                document.getElementById("movieTitle").innerHTML = obj1;
-                document.getElementById("moviePoster").src += obj2;
-                document.getElementById("movieOverview").innerHTML = obj3;
+                //document.getElementById("movieTitle").innerHTML = obj1;
+                //document.getElementById("moviePoster").src += obj2;
+                //document.getElementById("movieOverview").innerHTML = obj3;
 
             });
 
@@ -97,21 +119,21 @@
 <br>
 
 <!-- Edw emfanizontai stoixeia tainias -->
-<div  id="results" class="display">
+<!--<div  id="results" class="display">
 
     <div class="c1">
         <p class="p-title" id="movieTitle"></p><br>
     </div><br>
     <div class="c2">
-        <img class="poster" id="moviePoster" ><br> <!--src="https://image.tmdb.org/t/p/w500/"-->
+        <img class="poster" id="moviePoster" ><br>
     </div><br>
     <div class="c3">
         <p class="p-overview" id="movieOverview"></p>
     </div>
 
-</div>
+</div>-->
 
-
+<!--src="https://image.tmdb.org/t/p/w500/"-->
 
 
 
