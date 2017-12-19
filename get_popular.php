d3309e81ae166570480cb2852d7e3eb54ba14c87
gatzaras <gatzaras@gmail.com>
2017-12-19 22:53:38 +0200

    link_correction: Call to server for position correction station

--- get_popular.php
+++ get_popular.php
@@ -1,8 +1,8 @@
 <?php
 /**
 // link_correction: Call to server for position correction station
- Εντοπισμός των δημοφιλέστερων ταινοών απο το API της The Movie Database (TMDb)
+ Εντοπισμός των δημοφιλέστερων ταινιών απο το API της The Movie Database (TMDb)
  */
-$response = file_get_contents ('https://api.themoviedb.org/3/movie/popular?api_key=b5456db86ace1556b60313e04972fc9f&language=en-US&page=1');
-
+    $response = file_get_contents ('https://api.themoviedb.org/3/movie/popular?api_key=b5456db86ace1556b60313e04972fc9f&language=en-US&page=1');
+    $popular= json_decode($response,true);
 ?>
