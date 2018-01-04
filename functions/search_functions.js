3601c261eb51711cb3947c809b30198aecb543d1
kouskou <32963289+kouskou@users.noreply.github.com>
2018-01-04 12:04:05 +0200

    More details about movies. Improved further the message about not finding a movie

--- functions/search_functions.js
+++ functions/search_functions.js
@@ -19,62 +19,67 @@ function movieSearch(x){
 
         console.log(response);
         var i, id;
-        var obj1, obj2, obj3, obj4;
+        var obj1, obj2, obj3;
         var myResult1, myResult2, myResult3="";
         var movieWrapper;
-        var obj5 = "Ooops! we couldn't find any movies that matches your search!";
+        var obj5 = "Ooops! we couldn't find any movies that match your search!";
 
         //checking -> if the users's search doesn't mach any results, show an informative message to the user
         if(response.total_results === 0){
-            obj4 = document.createElement("P");
-            obj4.class = "obj4Class";
-            obj4.innerHTML = obj5;
-            document.body.appendChild(obj4);
+            document.getElementById("noResultsP").innerHTML = obj5;
+            $("#showMoreButton").hide();
+        }
+        else{
+            for(i=0; i<x; i++) {
+
+                $("#noResultsP").empty(); //there is a movie tha match the user's input, so make this message empty
+
+                //Making a DIV dynamically. In this DIV, the title, poster and overview of a movie is being displayed.
+                movieWrapper = document.createElement("DIV");
+                movieWrapper.id = "displayDiv";
+                movieWrapper.style.textAlign = "center";
+                movieWrapper.style.fontSize = "100%";
+                movieWrapper.style.backgroundColor = "#f2f2f2";
+                movieWrapper.style.margin = "auto";
+                movieWrapper.style.width = "70%";
+                movieWrapper.style.border = "3px solid #73AD21";
+                document.getElementById("results").appendChild(movieWrapper);
+
+                id = response.results[i].id; //get id from movie
+
+                //creating an <a> for the title and appending it into the DIV
+                obj1 = response.results[i].original_title; //getting the original_title from table results that is in the returned Json data
+                myResult1 = document.createElement("A");
+                myResult1.innerHTML = obj1; //insert movie title
+                myResult1.href = "show_movie.php?id=" + id; //active link for full overview of movie --> send id of movie to page show_movie
+                myResult1.style.fontSize = "36px";
+                document.getElementById("displayDiv").appendChild(myResult1);
+
+                document.getElementById("displayDiv").innerHTML += "<br>"; //break lines between movie title and image
+                document.getElementById("displayDiv").innerHTML += "<br>";
+
+                //creating an <img> for the poster and appending it into the DIV
+                obj2 = response.results[i].poster_path; //getting the poster_path from table results that is in the returned Json data
+                myResult2 = document.createElement("IMG");
+                myResult2.src = "https://image.tmdb.org/t/p/w500/";
+                myResult2.src += obj2;
+                myResult2.style.height = "20%";
+                myResult2.style.width = "20%";
+                document.getElementById("displayDiv").appendChild(myResult2);
+
+                //creating a <p> for the overview and appending it into the DIV
+                obj3 = response.results[i].overview; //getting the overview from table results that is in the returned Json data
+                myResult3 = document.createElement("P");
+                myResult3.innerHTML = obj3;
+                document.getElementById("displayDiv").appendChild(myResult3);
+            }
+
+            $("#showMoreButton").show();
         }
 
-        for(i=0; i<x; i++) {
 
-            //Making a DIV dynamically. In this DIV, the title, poster and overview of a movie is being displayed.
-            movieWrapper = document.createElement("DIV");
-            movieWrapper.id = "displayDiv";
-            movieWrapper.style.textAlign = "center";
-            movieWrapper.style.fontSize = "100%";
-            movieWrapper.style.backgroundColor = "#f2f2f2";
-            movieWrapper.style.margin = "auto";
-            movieWrapper.style.width = "70%";
-            movieWrapper.style.border = "3px solid #73AD21";
-            document.getElementById("results").appendChild(movieWrapper);
 
-            id = response.results[i].id; //get id from movie
-
-            //creating an <a> for the title and appending it into the DIV
-            obj1 = response.results[i].original_title; //getting the original_title from table results that is in the returned Json data
-            myResult1 = document.createElement("A");
-            myResult1.innerHTML = obj1; //insert movie title
-            myResult1.href = "show_movie.php?id=" + id; //active link for full overview of movie --> send id of movie to page show_movie
-            myResult1.style.fontSize = "36px";
-            document.getElementById("displayDiv").appendChild(myResult1);
-
-            document.getElementById("displayDiv").innerHTML += "<br>"; //break lines between movie title and image
-            document.getElementById("displayDiv").innerHTML += "<br>";
-
-            //creating an <img> for the poster and appending it into the DIV
-            obj2 = response.results[i].poster_path; //getting the poster_path from table results that is in the returned Json data
-            myResult2 = document.createElement("IMG");
-            myResult2.src = "https://image.tmdb.org/t/p/w500/";
-            myResult2.src += obj2;
-            myResult2.style.height = "20%";
-            myResult2.style.width = "20%";
-            document.getElementById("displayDiv").appendChild(myResult2);
-
-            //creating a <p> for the overview and appending it into the DIV
-            obj3 = response.results[i].overview; //getting the overview from table results that is in the returned Json data
-            myResult3 = document.createElement("P");
-            myResult3.innerHTML = obj3;
-            document.getElementById("displayDiv").appendChild(myResult3);
-        }
-
-        document.getElementById("showMoreButton").style.display = "block";
+        //document.getElementById("showMoreButton").style.display = "block";
     });
 }
 
@@ -95,9 +100,10 @@ function showMovieWithID(id){
     $.ajax(settings).done(function (response) {
 
         console.log(response);
-        var obj1, obj2, obj3;
-        var myResult1, myResult2, myResult3="";
-        var movieWrapper;
+        var obj1, obj2, obj3, obj4, obj5, obj6;
+        var myResult1, myResult2, myResult3, myResult4, myResult5, myResult6;
+        var movieWrapper
+        var i;
 
         //Making a DIV dynamically. In this DIV, the title, poster and overview of a movie is being displayed.
         movieWrapper = document.createElement("DIV");
@@ -131,6 +137,28 @@ function showMovieWithID(id){
         myResult3 = document.createElement("P");
         myResult3.innerHTML = obj3;
         document.getElementById("displayDiv").appendChild(myResult3);
+
+        obj4 = response.release_date;
+        myResult4 = document.createElement("P");
+        myResult4.innerHTML = "Release Date: " + obj4; //insert movie title
+        myResult4.style.fontSize = "20px";
+        document.getElementById("displayDiv").appendChild(myResult4);
+
+        obj5 = response.popularity;
+        myResult5 = document.createElement("P");
+        myResult5.innerHTML = "Popularity: " + obj5; //insert movie title
+        myResult5.style.fontSize = "20px";
+        document.getElementById("displayDiv").appendChild(myResult5);
+
+        for(i=0; i<response.genres.length; i++){
+            obj6 = response.genres[i].name;
+            myResult6 = document.createElement("P");
+            myResult6.innerHTML = "Genres: " + obj6; //insert movie title
+            myResult6.style.fontSize = "20px";
+            document.getElementById("displayDiv").appendChild(myResult6);
+        }
+
+
     });
 }
 
