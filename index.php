457305688414e02f4b50d59e698677031999fd37
NickELgr <32963580+NickELgr@users.noreply.github.com>
2018-01-06 00:33:43 +0200

    -Added bootstrap for header graphics -All pages call header_menu for their header -Header_menu contains: TMDB logo, home button, navigation bar -At all pages added responsive code for mobile

--- index.php
+++ index.php
@@ -5,52 +5,52 @@
       <title>MoviesInfo</title>
       <link rel="stylesheet" type="text/css" href="MoviesInfo.css">
       <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- mobile responsive -->
       <script src="jquery-3.2.1.js"></script>
       <script src="functions/search_functions.js"></script>
       <script src="functions/redirects.js"></script>
       <script src="functions/enter_key.js"></script>
+      <!-- Bootstrap library -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 
 
   </head>
 
 
 <body>
-
-<!-- Εδώ βρίσκονται ο τίτλος και το logo που πατώντας το μας πηγαινει στο site τους -->
-<div class = "TheHeader">
-    <a href="https://www.themoviedb.org/" target="_blank">
-        <img src="https://www.themoviedb.org/assets/static_cache/02a9430b88975cae16fcfcc9cf7b5799/images/v4/logos/primary-green.svg" >
-    </a>
-    <h1>Movie Info</h1><br>
-</div>
-
-<p>Learn all about your favourite movies!</p><br>
-
-<!-- Εδώ βρίσκεται το search box και το button για την εκκίνηση αναζήτησης -->
-<div class = "TheSearch">
-    <form>
-        <input type="text" id="search1" name="search" autocomplete="off" placeholder="Start Searching!" onkeydown="enterSearch(event)">
-        <input type="button" id="submit" value="Submit" onclick="movieSearch(3)">
-    </form>
-</div><br>
-
-<!-- Εδώ βρίσκεται το button για την εμφάνιση των popular movies -->
-<div class = "TheSearch">
-    <p>See all the recent and most popular movies</p>
-    <input type="button" id="showPopular" value="Popular Movies" onclick="redirectToIndex2()">
-    <p>See the movies that are currently playing in theaters</p>
-    <input type="button" id="showCurrent" value="Movies in Theaters" onclick="redirectToIndex3()">
-</div><br>
-
-<div id="results">
-
-</div>
+<!--header for all pages made by bootstrap -->
+<?php include('header_menu.php'); ?>
 
 
-<input type="button" id="showMoreButton" value="Show More Movies!" style = "display:none" onclick="movieSearch(100)">
+    <h1 class="title text-center">Movies Info</h1>
 
-<p id="noResultsP"></p>
+    <p>Learn all about your favourite movies!</p><br>
 
+    <!-- Εδώ βρίσκεται το search box και το button για την εκκίνηση αναζήτησης -->
+    <div class = "TheSearch">
+        <form>
+            <input type="text" id="search1" name="search" autocomplete="off" placeholder="Start Searching!" onkeydown="enterSearch(event)">
+            <input type="button" id="submit" value="Submit" onclick="movieSearch(3)">
+        </form>
+    </div><br>
+
+    <!-- Εδώ βρίσκεται το button για την εμφάνιση των popular movies -->
+    <div class = "TheSearch">
+        <p>See all the recent and most popular movies</p>
+        <input type="button" id="showPopular" value="Popular Movies" onclick="redirectToIndex2()">
+        <p>See the movies that are currently playing in theaters</p>
+        <input type="button" id="showCurrent" value="Movies in Theaters" onclick="redirectToIndex3()">
+    </div><br>
+
+    <div id="results">
+
+    </div>
+
+
+    <input type="button" id="showMoreButton" value="Show More Movies!" style = "display:none" onclick="movieSearch(100)">
+
+    <p id="noResultsP"></p>
 </body>
 
 </html>
